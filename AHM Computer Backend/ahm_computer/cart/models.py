fdd8377c3fde0464a8600c70a36262ccd96266f3
mowuj <mowuj92@gmail.com>
2024-02-06 21:00:03 +0600

    order model

--- AHM Computer Backend/ahm_computer/cart/models.py
+++ AHM Computer Backend/ahm_computer/cart/models.py
@@ -26,3 +26,21 @@ class CartProduct(models.Model):
         return "Cart: " + str(self.cart.id) + "CartProduct: " + str(self.id)
 
 
+class Order(models.Model):
+    cart = models.OneToOneField(Cart, on_delete=models.CASCADE)
+    ordered_by = models.CharField(max_length=250)
+    shipping_address = models.CharField(max_length=250)
+    phone = models.CharField(max_length=13)
+    email = models.EmailField(null=True, blank=True)
+    subtotal = models.PositiveIntegerField()
+    discount = models.PositiveIntegerField()
+    total = models.PositiveIntegerField()
+    order_status = models.CharField(max_length=250, choices=ORDER_STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(
+        max_length=20, choices=METHOD, default="Cash On Delivery")
+    payment_completed = models.BooleanField(
+        default=False, null=True, blank=True)
+
+    def __str__(self):
+        return "Order: " + str(self.id)
